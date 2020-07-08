8ac6462b79f7439015e8aadfdfc58fbdca092e35
bunnyreddy17303 <bunnyreddy830@gmail.com>
2020-07-08 21:22:26 +0530

    created React Weather App

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 import "weather-icons/css/weather-icons.css";
 
-const Api_Key = "429736441cf3572838aa10530929f7cd";
+const Api_Key = "4c93327739d639573eaaef3004f89ec0";
 
 class App extends React.Component {
   constructor() {
